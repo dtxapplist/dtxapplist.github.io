a9ef624a8a77a07743c8803e61c3cbdfe1104d21
dtxapplist <archiver0101@gmail.com>
2025-08-10 23:30:14 +0300

    Update apps.js

--- apps.js
+++ apps.js
@@ -2,23 +2,39 @@ const apps = [
     {
         name: "Discord",
         icon: "icons/discord.png",
-        supported: true
+        supported: true,
+        install: `# Debian/Ubuntu
+sudo apt install discord
+
+# Arch
+sudo pacman -S discord`
     },
     {
         name: "Adobe Photoshop",
         icon: "icons/photoshop.png",
         supported: false,
-        alt: "GIMP"
+        alt: "GIMP",
+        install: `# GIMP Kurulumu (Photoshop alternatifi)
+sudo apt install gimp
+sudo pacman -S gimp`
     },
     {
         name: "Steam",
         icon: "icons/steam.png",
-        supported: true
+        supported: true,
+        install: `# Debian/Ubuntu
+sudo apt install steam
+
+# Arch
+sudo pacman -S steam`
     },
     {
         name: "Microsoft Office",
         icon: "icons/office.png",
         supported: false,
-        alt: "LibreOffice"
+        alt: "LibreOffice",
+        install: `# LibreOffice Kurulumu (Office alternatifi)
+sudo apt install libreoffice
+sudo pacman -S libreoffice`
     }
 ];
