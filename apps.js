85ad4d9254818660ca7f7dc2803d63a7a95ca864
dtxapplist <archiver0101@gmail.com>
2025-08-10 23:54:16 +0300

    Update apps.js

--- apps.js
+++ apps.js
@@ -3,95 +3,76 @@ const apps = [
         name: "Discord",
         icon: "🎮",
         supported: true,
-        install: `# Debian/Ubuntu
-sudo apt install discord
-
-# Arch
-sudo pacman -S discord
-
-# Flatpak (tüm dağıtımlar için)
-flatpak install flathub com.discordapp.Discord`
+        install: {
+            "Debian/Ubuntu": "sudo apt install discord",
+            "Arch": "sudo pacman -S discord",
+            "Fedora": "sudo dnf install discord",
+            "Flatpak": "flatpak install flathub com.discordapp.Discord"
+        }
     },
     {
         name: "Adobe Photoshop",
         icon: "🎨",
         supported: false,
         alt: "GIMP",
-        install: `# GIMP Kurulumu (Photoshop alternatifi)
-
-# Debian/Ubuntu
-sudo apt install gimp
-
-# Arch
-sudo pacman -S gimp
-
-# Flatpak
-flatpak install flathub org.gimp.GIMP`
+        install: {
+            "Debian/Ubuntu": "sudo apt install gimp",
+            "Arch": "sudo pacman -S gimp",
+            "Fedora": "sudo dnf install gimp",
+            "Flatpak": "flatpak install flathub org.gimp.GIMP"
+        }
     },
     {
         name: "Steam",
         icon: "🎯",
         supported: true,
-        install: `# Debian/Ubuntu
-sudo apt install steam
-
-# Arch
-sudo pacman -S steam
-
-# Flatpak
-flatpak install flathub com.valvesoftware.Steam`
+        install: {
+            "Debian/Ubuntu": "sudo apt install steam",
+            "Arch": "sudo pacman -S steam",
+            "Fedora": "sudo dnf install steam",
+            "Flatpak": "flatpak install flathub com.valvesoftware.Steam"
+        }
     },
     {
         name: "Microsoft Office",
         icon: "📝",
         supported: false,
         alt: "LibreOffice",
-        install: `# LibreOffice Kurulumu (Office alternatifi)
-
-# Debian/Ubuntu
-sudo apt install libreoffice
-
-# Arch
-sudo pacman -S libreoffice-fresh
-
-# Flatpak
-flatpak install flathub org.libreoffice.LibreOffice`
+        install: {
+            "Debian/Ubuntu": "sudo apt install libreoffice",
+            "Arch": "sudo pacman -S libreoffice-fresh",
+            "Fedora": "sudo dnf install libreoffice",
+            "Flatpak": "flatpak install flathub org.libreoffice.LibreOffice"
+        }
     },
     {
         name: "Visual Studio Code",
         icon: "💻",
         supported: true,
-        install: `# VS Code Kurulumu
-
-# Debian/Ubuntu (Microsoft deposu)
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+        install: {
+            "Debian/Ubuntu": `wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
-sudo apt update
-sudo apt install code
-
-# Arch (AUR)
-yay -S visual-studio-code-bin
-
-# Flatpak
-flatpak install flathub com.visualstudio.code`
+sudo apt update && sudo apt install code`,
+            "Arch": "yay -S visual-studio-code-bin",
+            "Fedora": `sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf check-update && sudo dnf install code`,
+            "Flatpak": "flatpak install flathub com.visualstudio.code"
+        }
     },
     {
         name: "Spotify",
         icon: "🎵",
         supported: true,
-        install: `# Spotify Kurulumu
-
-# Debian/Ubuntu
-curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+        install: {
+            "Debian/Ubuntu": `curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt update
-sudo apt install spotify-client
-
-# Arch (AUR)
-yay -S spotify
-
-# Flatpak
-flatpak install flathub com.spotify.Client`
+sudo apt update && sudo apt install spotify-client`,
+            "Arch": "yay -S spotify",
+            "Fedora": `sudo dnf config-manager --add-repo=https://negativo17.org/repos/fedora-spotify.repo
+sudo dnf install spotify-client`,
+            "Flatpak": "flatpak install flathub com.spotify.Client"
+        }
     }
 ];
