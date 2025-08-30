6f0d5a8e080d82cbadc0b25fceb91b9e598cb501
dtxapplist <archiver0101@gmail.com>
2025-08-30 22:46:13 +0300

    Delete api/simple.js

--- api/simple.js
+++
@@ -1,8 +0,0 @@
-// api/simple.js - En basit test
-
-export default function handler(request) {
-    return new Response('Hello World!', {
-        status: 200,
-        headers: { 'Content-Type': 'text/plain' }
-    });
-}
